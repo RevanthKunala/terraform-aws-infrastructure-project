d556357016e5bf4da821af5a125ea41ec5d66a21
RevanthKunala <151742373+RevanthKunala@users.noreply.github.com>
2026-06-16 01:52:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,3 +41,10 @@ resource "aws_instance" "webserver" {
     Name = "Terraform-WebServer"
   }
 }
+terraform {
+  backend "s3" {
+    bucket = "your-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
